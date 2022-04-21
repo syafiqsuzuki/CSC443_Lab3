38209c5eb7e3964af8b826a1dc01224d3f2ed05c
Syafiqm <syafiq4000@gmail.com>
2022-04-21 10:54:09 +0800

    ninth.commit

---
+++ sttring.php
@@ -0,0 +1,20 @@
+<html> <body>
+
+<?php
+
+function add ($x, $y) {
+
+$sum = $x + $y; return $sum;
+
+}
+
+$numl = 11;
+
+$num2 = 15;
+
+$summ= add ($num1, $num2) echo "$num1 + $num2 ?>
+
+$summ;
+
+</body> 
+</html>
