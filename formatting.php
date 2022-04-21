f2c0e96d168acb465fa66edf572175637e587bb5
Syafiqm <syafiq4000@gmail.com>
2022-04-21 10:45:07 +0800

    sixth.commit

---
+++ formatting.php
@@ -0,0 +1,18 @@
+<html>
+
+<body> <?php
+
+printf("%.2f", 123.456);
+
+echo "<br />";
+
+printf ("8.10f", 123.456); echo "<br />";
+
+printf ("%.0f", 123.456); echo "<br />";
+
+echo number_format("2500000",2);
+
+?>
+
+</body> 
+</html>
