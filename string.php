da36c4149ac3e59b348d47e2c871831229aa994a
Syafiqm <syafiq4000@gmail.com>
2022-04-21 10:47:10 +0800

    seventh.commit

---
+++ string.php
@@ -0,0 +1,20 @@
+<html>
+
+<body> <?php
+
+$txtl "Hello there, World!";
+
+$txt2 = "Have a nice day!"; $greet $txtl. "" . $txt2
+
+echo $greet; ?> Oecho "<br />";
+
+echo strlen ("Hello world!");
+
+echo "<br />"; echo strpos ("Hello world!", "world");
+
+echo "<br />";
+
+?>
+
+</body>
+</html>
