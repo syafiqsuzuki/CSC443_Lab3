db05a66bc84f1481f3b8fb6d5aa5a4365468e5ae
Syafiqm <syafiq4000@gmail.com>
2022-04-21 10:15:55 +0800

    third.commit

---
+++ variable.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+<?php $username = "Fred Smith".
+
+echo $username; echo "<br />";
+
+Scurrent_user= $username;
+
+echo $current_user; echo "<br />";
+
+$num1 = 10.5;
+
+echo $numl;
+
+?>
+
+</body> </html>
